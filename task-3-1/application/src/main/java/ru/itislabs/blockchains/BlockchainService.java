e055b2d36a16cf125ea9491d162b2f74fd3ab94f
azazaurus <1maxovmax!@gmail.com>
2023-11-27 02:15:49 +0400

    Defined interface of blockchain service

---
+++ task-3-1/application/src/main/java/ru/itislabs/blockchains/BlockchainService.java
@@ -0,0 +1,19 @@
+package ru.itislabs.blockchains;
+
+public class BlockchainService {
+	public int appendBlock(String data) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int appendArbitraryBlock(Block block) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Block readBlock(int id) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean verifyBlock(int id) {
+		throw new UnsupportedOperationException();
+	}
+}
